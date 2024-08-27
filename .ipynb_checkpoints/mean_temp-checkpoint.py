412632558f5c9dc8ab998ea9cad1fb9ab1c20748
JorgeRomero1 <106196874+JorgeRomero1@users.noreply.github.com>
2024-08-27 14:57:01 -0500

    Create variable z
    Mathematical operators were assigned to variable z for calculating mean temperature using variable x as the high temperature and y as the low temperature

--- .ipynb_checkpoints/mean_temp-checkpoint.py
+++ .ipynb_checkpoints/mean_temp-checkpoint.py
@@ -3,20 +3,20 @@
 
 """
     File name: mean_temp.py
-    Author: Jorge Romero Soler
+    Author: ENTER YOUR  NAME
     Description:  Calculates mean daily temperature from two user defined variables representing a daily high (x) and low (y) temperature
-    Date created: 08/27/2024 in MM/DD/YYYY FORMAT (e.g., 08/23/2023)
+    Date created: ENTER TODAY'S DATE in MM/DD/YYYY FORMAT (e.g., 08/23/2023)
     Python Version: 3.9.16
 """
 
 # Assign variable x the value for the previous day's high temperature
-x = 35
+x = 
 
 # Assign variable y the value for the previous day's low temperature
-y = 22 
+y =  
 
 # Create variable z and use mathematical operators to calculate the mean temperature
-z = (x+y)/2
+z = 
 
 # Use this custom statement that prints the result to screen
 print("Yesterday's mean daily temperature was {0} degrees.".format(z))
